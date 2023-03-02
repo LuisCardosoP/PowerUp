37f13214c46960d9f5ba4cf3020b57a46e17d2f9
LuisCardoso <luiscardosoyusty@gmail.com>
2023-03-01 22:56:15 -0500

    estructura security

---
+++ powerup-user/src/main/java/com/powerup/user/infraestructure/out/jpa/repository/IUserAuthRepository.java
@@ -0,0 +1,15 @@
+package com.powerup.user.infraestructure.out.jpa.repository;
+
+import com.powerup.user.infraestructure.out.jpa.entity.UserAuthEntity;
+import com.powerup.user.infraestructure.out.jpa.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface IUserAuthRepository extends JpaRepository<UserAuthEntity, Long> {
+
+    UserAuthEntity findOneByEmail(String emailUser);
+
+}
